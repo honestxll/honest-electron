417f9a3bc7ece83accd11dcdd283e3ad3670d96a
honest <honestxll@gmail.com>
2019-08-27 20:06:30 +0800

    创建一个html文档做为electron的窗口

--- main.js
+++ main.js
@@ -3,8 +3,14 @@ const { app, BrowserWindow } = require("electron");
 let win;
 
 const createWindow = () => {
-  win = new BrowserWindow();
-  win.loadURL("https://can-leading.cn");
+  win = new BrowserWindow({
+    width: 960,
+    height: 600,
+    minWidth: 830,
+    minHeight: 500,
+    backgroundColor: "#f8f8f8"
+  });
+  win.loadURL(`file://${__dirname}/main.html`);
 };
 
 app.on("ready", createWindow);
