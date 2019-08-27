870814b16a2540b49f70e95b97750673a4f33d11
honest <honestxll@gmail.com>
2019-08-27 20:31:51 +0800

    针对mac系统，用户关闭应用的时候不会完全关闭，可以再点一下应用图标，再次启动应用

--- main.js
+++ main.js
@@ -20,7 +20,25 @@ const createWindow = () => {
     },
     backgroundColor: '#f8f8f8'
   })
+
   win.loadURL(`file://${__dirname}/main.html`)
+
+  win.on('close', () => {
+    win = null
+  })
 }
 
 app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // 当用户关闭应用的时候，如果用户的操作系统不是 mac 系统，则会退出应用
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win == null) {
+    createWindow()
+  }
+})
